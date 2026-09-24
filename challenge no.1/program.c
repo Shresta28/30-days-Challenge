69472a197470d9956bfa470fc7cdd200d993d16c
Shresta28 <akulasaishresta@gmail.com>
2026-09-24 12:01:34 +0530

    Fix total marks calculation in program.c
    Fixed the calculation of total marks by adding the missing '+' operators.

--- challenge no.1/program.c
+++ challenge no.1/program.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+        int main(){
+        int marks1,marks2,marks3,marks4,marks5;
+        float total,avg,percentage;
+printf("Enter the five subjects marks\n");
+scanf("%d%d%d%d%d", &marks1, &marks2, &marks3, &marks4, &marks5);
+total = marks1+marks2+marks3+marks4+marks5;
+avg = total/5;
+percentage = (total/500)*100;
+        printf("The total is: %f\n", total);
+        printf("The average is: %f\n", avg);
+        printf("The percentage is: %f\n", percentage);
+return 0;
+}
